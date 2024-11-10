40cb51bab4668c36929318b4d9668ffc67f8a4cd
suvani-ctrl <sssubbbi1234@gmail.com>
2024-11-10 10:48:27 +0545

    Update Freq_Analysis.c
    Updated Logic

--- Freq_Analysis.c
+++ Freq_Analysis.c
@@ -3,13 +3,15 @@
 #include <string.h>
 #include <ctype.h>  
 
+
 void slice(char *words) {
     system("clear");  
-    printf("Enter string/letters for analysis:\n");
+    printf("Enter your paragraph for analysis:\n");
     fgets(words, 1000, stdin);  
 }
 
-int count(char *words) {
+
+int count_characters(char *words) {
     int charCount = 0;
     for (int i = 0; words[i] != '\0'; i++) {
         if (isalpha(words[i])) { 
@@ -19,7 +21,9 @@ int count(char *words) {
     return charCount;
 }
 
-char* sorting(char *words, int length) {
+
+void sort_string(char *words) {
+    int length = strlen(words);
     for (int i = 0; i < length - 1; i++) {
         for (int j = 0; j < length - i - 1; j++) {
             if (words[j] > words[j + 1]) {
@@ -29,7 +33,6 @@ char* sorting(char *words, int length) {
             }
         }
     }
-    return words;  
 }
 
 void frequency(char *words, int *count) {
@@ -40,6 +43,7 @@ void frequency(char *words, int *count) {
     }
 }
 
+
 void percentage_conversion(int *count, int total_chars) {
     printf("Character frequencies and percentages:\n");
     for (int i = 0; i < 256; i++) {
@@ -47,8 +51,19 @@ void percentage_conversion(int *count, int total_chars) {
             double percentage = (count[i] / (double)total_chars) * 100;
             printf("'%c' = %d (%.2f%%)\n", i, count[i], percentage);
         }
-      
     }
+}
+
+
+void caesar_cipher(char *words, int shift) {
+    printf("Converting it to Caesar cipher:\n");
+    for (int i = 0; words[i] != '\0'; i++) {
+        if (isalpha(words[i])) {
+            char offset = isupper(words[i]) ? 'A' : 'a';
+            words[i] = ((words[i] - offset + shift) % 26) + offset;
+        }
+    }
+    printf("Ciphered Text: %s\n", words);
 }
 
 int main() {
@@ -56,14 +71,18 @@ int main() {
     int count_arr[256] = {0}; 
 
     slice(words);  
-    int charCount = count(words);  
-    printf("The total character count is: %d\n", charCount);  
+    int charCount = count_characters(words);  
+    printf("The total alphabetic character count is: %d\n", charCount);  
     
-    int length = strlen(words);
-    printf("Sorted String: %s\n", sorting(words, length));
+    sort_string(words);
+    printf("Sorted String: %s\n", words);
 
     frequency(words, count_arr);  
-    percentage_conversion(count_arr, charCount); 
+    percentage_conversion(count_arr, charCount);
+
+  
+    int shift = 3;
+    caesar_cipher(words, shift);
 
     return 0;
-} 
+}
